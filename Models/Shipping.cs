bd4e9f2bb8719a1c7faed2ac6e960d3870bbf670
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-10-05 23:44:39 -0500

    feat(Shipping): add attributes
    Added:
    - Weight
    - Length
    - Width
    - Height
    - SourceAddress
    - SourceCity
    - DestinationAddress
    - DestinationCity
    - RegisterDate

--- Models/Shipping.cs
+++ Models/Shipping.cs
@@ -14,7 +14,20 @@ namespace CargoTrack_Backend.Models
         public int ShippingId { get; set; }
         public string Description { get; set; }
         public int CustomerId { get; set; }
+        
         [ForeignKey("CustomerId")]
         public virtual Customer Customer { get; set; }
+        
+        public double Weight { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public string SourceAddress { get; set; }
+        public string SourceCity { get; set; }
+        public string DestinationAddress { get; set; }
+        public string DestinationCity { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RegisterDate { get; set; }
     }
 }
