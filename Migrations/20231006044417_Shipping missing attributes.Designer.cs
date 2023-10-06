e73270787b3fff087a3e7515e4839c1f97b54c6b
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-10-05 23:45:31 -0500

    feat(Migrations): Shipping missing attributes migrations

--- Migrations/20231006044417_Shipping missing attributes.Designer.cs
+++ Migrations/20231006044417_Shipping missing attributes.Designer.cs
@@ -4,6 +4,7 @@ using CargoTrack_Backend.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CargoTrack_Backend.Migrations
 {
     [DbContext(typeof(ShippingDbContext))]
-    partial class ShippingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231006044417_Shipping missing attributes")]
+    partial class Shippingmissingattributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
