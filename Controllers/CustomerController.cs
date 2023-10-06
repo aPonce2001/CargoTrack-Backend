1ad8b5aa449d1bed2cdb034f51ffa5760138d76e
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-10-06 00:13:58 -0500

    feat(Controllers): add Customer Controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CargoTrack_Backend.Database;
+using CargoTrack_Backend.Models;
+
+namespace CargoTrack_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private ShippingDbContext _context;
+
+        public CustomerController(ShippingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return _context.Customers.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Customer customer)
+        {
+            _context.Customers.Add(customer);
+        }
+    }
+}
