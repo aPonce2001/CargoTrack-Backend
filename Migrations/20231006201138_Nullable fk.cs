6d13bc3e7046fc1fc7be0da610438524a58a7624
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-10-06 15:20:29 -0500

    fix(Shipping): make nullable fk

---
+++ Migrations/20231006201138_Nullable fk.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CargoTrack_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nullablefk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shipping_Customer_CustomerId",
+                table: "Shipping");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Shipping",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shipping_Customer_CustomerId",
+                table: "Shipping",
+                column: "CustomerId",
+                principalTable: "Customer",
+                principalColumn: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Shipping_Customer_CustomerId",
+                table: "Shipping");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Shipping",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Shipping_Customer_CustomerId",
+                table: "Shipping",
+                column: "CustomerId",
+                principalTable: "Customer",
+                principalColumn: "CustomerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
