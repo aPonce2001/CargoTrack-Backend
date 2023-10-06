f6f8f6a6096f1a8dd7525e2b62145a540c88ac4c
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-10-06 00:41:44 -0500

    fix(Program): fix CORS policy

--- Program.cs
+++ Program.cs
@@ -32,6 +32,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
